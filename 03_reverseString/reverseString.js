c840a07685e3f744530be4fadd3e32ec841e7939
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-22 15:40:04 -0700

    solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str.length === 0 || str.length === 1){
+        return str;
+    }
+    let left = 0;
+    let right = str.length - 1;
+    const res = new Array(str.length);
+    while (left <= right){
+        if (left !== right){
+            res[left] = str[right];
+            res[right] = str[left];
+        }
+        else {
+            res[left] = str[left];
+        }
+        left++;
+        right--;
+    }
+    return res.join("");
 };
 
 // Do not edit below this line
