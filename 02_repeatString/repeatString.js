f445eda81546767a49bfc926b44449e3b6d2ace5
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-22 15:04:26 -0700

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputStr, repeatCounter) {
+    if (repeatCounter < 0){
+        return "ERROR";
+    }
+    let outputStr = ""
+    while (repeatCounter !== 0){
+        outputStr += inputStr;
+        repeatCounter--;
+    }
+    return outputStr;
 };
 
 // Do not edit below this line
