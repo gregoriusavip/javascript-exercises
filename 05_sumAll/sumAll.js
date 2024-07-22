ecd15fd6d9850fcbda6a00b1576f6bebcbdffb8d
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-22 16:31:23 -0700

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+    if(typeof intOne !== "number" 
+        || typeof intTwo !== "number" 
+        || intOne < 0 
+        || intTwo < 0 
+        || !Number.isInteger(intOne) 
+        || !Number .isInteger(intTwo)){
+        return 'ERROR';
+    }
+    let larger = intTwo;
+    let smaller = intOne;
+    if (intOne > intTwo){
+        larger = intOne;
+        smaller = intTwo;
+    }
+    let res = 0;
+    while (larger >= smaller){
+        res += smaller;
+        smaller++;
+    }
+    return res;
 };
 
 // Do not edit below this line
