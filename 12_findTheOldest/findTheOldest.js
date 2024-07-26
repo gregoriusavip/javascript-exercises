e15919ada48f17487702acf1a441f7c0a6beb425
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-25 21:52:04 -0700

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    if (!("yearOfDeath" in oldestPerson)) {
+      oldestPerson.yearOfDeath = new Date().getFullYear();
+    }
+    if (!("yearOfDeath" in currentPerson)) {
+      currentPerson.yearOfDeath = new Date().getFullYear();
+    }
+    const oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  }, people[0]);
 };
 
 // Do not edit below this line
